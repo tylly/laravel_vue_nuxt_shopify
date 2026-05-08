d30a7e3c3bd77f3c48499ac224fb9f04f7056683
tylly <jamestylladams@gmail.com>
2026-05-07 20:33:10 -0400

    add shadcn-vue

--- app_frontend/nuxt.config.ts
+++ app_frontend/nuxt.config.ts
@@ -1,24 +1,39 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   tailwindcss: {
-  config: {
-    theme: {
-      extend: {          // <-- add extend here
-        fontFamily: {
-          saira: ["Saira", "sans-serif"],
-          roboto: ["Roboto", "serif"],
+    config: {
+      theme: {
+        extend: {
+          // <-- add extend here
+          fontFamily: {
+            saira: ["Saira", "sans-serif"],
+            roboto: ["Roboto", "serif"],
+          },
         },
       },
     },
   },
-},
   runtimeConfig: {
     public: {
-      apiBase: process.env.API_BASE_URL || 'http://localhost:3000'
-    }
+      apiBase: process.env.API_BASE_URL || "http://localhost:3000",
+    },
   },
 
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/fonts']
-})
+  modules: ["@nuxtjs/tailwindcss", "@nuxt/fonts", "shadcn-nuxt"],
+  shadcn: {
+    /**
+     * Prefix for all the imported component.
+     * @default "Ui"
+     */
+    prefix: "",
+    /**
+     * Directory that the component lives in.
+     * Will respect the Nuxt aliases.
+     * @link https://nuxt.com/docs/api/nuxt-config#alias
+     * @default "@/components/ui"
+     */
+    componentDir: "@/components/ui",
+  },
+});

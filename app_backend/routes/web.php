c7954e73699ec2c681308b2a7d25bd1d0c1c6de1
tylly <jamestylladams@gmail.com>
2026-05-07 22:08:17 -0400

    reach out to shopify admin api for products

--- app_backend/routes/web.php
+++ app_backend/routes/web.php
@@ -11,8 +11,49 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'Dashboard')->name('dashboard');
 });
 
-Route::get('/test', function () {
+Route::get('/api/test', function () {
     return response()->json(['message' => 'Laravel and Nuxt are connected!']);
+});
+
+Route::get('/api/get-products', function () {
+    $access_token = env('SHOPIFY_PRIVATE_TOKEN');
+    $base_url = env('SHOPIFY_URL');
+    $api_version = env("SHOPIFY_API_VERSION");
+    $url = "$base_url/admin/api/$api_version/graphql.json";
+    $query = '{
+  products(first: 3) {
+    edges {
+      node {
+        id
+        title
+      }
+    }
+  }
+}';
+
+    $payload = json_encode(['query' => $query]);
+
+    $ch = curl_init($url);
+    curl_setopt_array($ch, [
+        CURLOPT_POST => true,
+        CURLOPT_POSTFIELDS => $payload,
+        CURLOPT_HTTPHEADER => [
+            'Content-Type: application/json',
+            // 'Accept: application/json',
+            "X-Shopify-Access-Token: $access_token",
+        ],
+        CURLOPT_RETURNTRANSFER => true
+    ]);
+
+    $response = curl_exec($ch);
+    $statusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    $curlError = curl_error($ch);
+    curl_close($ch);
+
+    if ($curlError) {
+        return response()->json(['error' => $curlError], 500);
+    }
+    return response()->json(json_decode($response));
 });
 
 require __DIR__ . '/settings.php';

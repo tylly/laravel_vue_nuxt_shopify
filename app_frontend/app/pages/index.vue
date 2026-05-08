b06ee9ed7f28a93758bf215a0728ffa4abd3aae1
tylly <jamestylladams@gmail.com>
2026-05-07 22:08:03 -0400

    reach out to shopify admin api for products

--- app_frontend/app/pages/index.vue
+++ app_frontend/app/pages/index.vue
@@ -4,19 +4,17 @@ console.log("Welcome page loaded");
 const config = useRuntimeConfig();
 const testfunction = async () => {
   try {
-    const { data } = await useFetch(`${config.public.apiBase}/test`);
+    const data = await $fetch(`${config.public.apiBase}/api/get-products`);
     console.log("route:" + config.public.apiBase);
-    console.log("API response:", data.value);
+    console.log("API response:", data);
   } catch (error) {
     console.error("Error fetching API:", error);
   }
 };
-
-testfunction();
 </script>
 <template>
   <div>
     <h1 class="text-2xs font-saira mb-20">Welcome to the Workout Planner App!</h1>
-    <Button class="bg-red-200">Test</Button>
+    <Button @click="testfunction()" variant="outline">Test</Button>
   </div>
 </template>

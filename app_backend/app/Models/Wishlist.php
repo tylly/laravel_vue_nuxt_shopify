9775cbf97e270b9232b470b84278bcc8279aa272
tylly <jamestylladams@gmail.com>
2026-05-10 20:20:15 -0400

    add relation

--- app_backend/app/Models/Wishlist.php
+++ app_backend/app/Models/Wishlist.php
@@ -3,8 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Wishlist extends Model
 {
-    //
+    protected $fillable = [
+        'product_id'
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }

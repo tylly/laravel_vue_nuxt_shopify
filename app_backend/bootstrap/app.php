55fb055f5b3b003f3e2d4432ee316f55bfa8b622
tylly <jamestylladams@gmail.com>
2026-05-11 20:45:14 -0400

    auth protect account page

--- app_backend/bootstrap/app.php
+++ app_backend/bootstrap/app.php
@@ -27,3 +27,4 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
+
--- app_frontend/app/pages/account/index.vue
+++ app_frontend/app/pages/account/index.vue
@@ -1,5 +1,8 @@
+<script setup lang="ts">
+definePageMeta({
+  middleware: ["sanctum:auth"],
+});
+</script>
 <template>
-    <h1>
-        Accounts page
-    </h1>
+  <h1>Accounts page</h1>
 </template>
